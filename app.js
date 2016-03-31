9eed9afffb2ceb18f965375e17d32ddcd22a45b9
Ashley Roux <angelo@elemental.co.za>
2016-03-31 21:51:07 +0200

    orientation test

--- app.js
+++ app.js
@@ -1,23 +1,87 @@
 var http = require('http'),
     fs = require('fs'),
-// NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/index.html');
+    path = require('path');
 
 // Send index.html to all requests
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+var app = http.createServer(function(request, response) {
+    var filePath = './public_html' + request.url;
+    if (filePath == './public_html/')
+        filePath = './public_html/index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 });
+
 
 // Socket.io server listens to our app
 var io = require('socket.io').listen(app);
 
+var keyListeners = {};
+
 // Emit welcome message on connection
 io.on('connection', function(socket) {
-    // Use socket to communicate with this particular client only, sending it it's own id
-    socket.on('keycode', function(val){
-        console.log(val);
-        io.emit('keyfetch',val);
+    var inputPassword = false;
+    var vrPassword = false;
+
+    socket.on('input_identifier',function(val){
+        inputPassword = val;
+    });
+    socket.on('vr_identifier',function(val){
+        vrPassword = val;
+        keyListeners[val] = socket;
+    });
+    socket.on('disconnect',function(){
+        if (vrPassword!==false)
+            delete(keyListeners[vrPassword]);
+    });
+
+    socket.on('keyup', function(val){
+        if (inputPassword!==false && keyListeners[inputPassword])
+            keyListeners[inputPassword].emit('keyup',val);
+    });
+    socket.on('keydown', function(val){
+        if (inputPassword!==false && keyListeners[inputPassword])
+            keyListeners[inputPassword].emit('keydown',val);
     });
 });
 
